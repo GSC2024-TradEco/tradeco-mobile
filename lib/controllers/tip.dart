499eb578aebd4712ca44807c32fba96290ee6d76
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-02-12 19:12:53 +0700

    Create TipController

---
+++ lib/controllers/tip.dart
@@ -0,0 +1,52 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:zero_waste_application/utils/api_endpoints.dart';
+import 'package:zero_waste_application/models/tip.dart';
+
+class tipController {
+  Future<List<Tip>> getAllTips(String token) async {
+    final Uri uri = Uri.parse(API.baseUrl + API.tipEndpoints.findAll);
+
+    try {
+      final http.Response response = await http.get(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonList = json.decode(response.body);
+        List<Tip> tips = jsonList.map((json) => Tip.fromJson(json)).toList();
+        return tips;
+      }
+    } catch (e) {
+      // Handle any network errors
+      print('Error: $e');
+    }
+  }
+
+  Future<Tip> getOneTip(int tipId, String token) async {
+    final Uri uri = Uri.parse(API.baseUrl + API.tipEndpoints.getOne(tipId));
+
+    try {
+      final http.Response response = await http.post(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 201) {
+        Map<String, dynamic> jsonResponse = json.decode(response.body);
+        Tip tip = Tip.fromJson(jsonResponse);
+        return tip;
+      }
+    } catch (e) {
+      // Handle any network errors
+      print('Error: $e');
+    }
+  }
+}
