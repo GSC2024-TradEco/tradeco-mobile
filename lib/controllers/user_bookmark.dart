c4b9ae9a0330436bd0412b77444c5da445ca8a24
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-02-12 18:59:18 +0700

    Create UserBookmarkController

---
+++ lib/controllers/user_bookmark.dart
@@ -0,0 +1,77 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:zero_waste_application/utils/api_endpoints.dart';
+import 'package:zero_waste_application/models/project.dart';
+
+class UserBookmarkController {
+  Future<List<Project>> getAllBookmarks(String token) async {
+    final Uri uri = Uri.parse(API.baseUrl + API.bookmarkEndpoints.findAll);
+
+    try {
+      final http.Response response = await http.get(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        List<dynamic> jsonList = json.decode(response.body);
+        List<Project> projects =
+            jsonList.map((json) => Project.fromJson(json)).toList();
+        return projects;
+      }
+    } catch (e) {
+      // Handle any network errors
+      print('Network error: $e');
+    }
+  }
+
+  Future<Project> createOneBookmark(int projectId, String token) async {
+    final Uri uri = Uri.parse(API.baseUrl + API.bookmarkEndpoints.createOne);
+    Map<String, dynamic> body = {'projectId ': projectId};
+
+    try {
+      final http.Response response = await http.post(
+        uri,
+        body: jsonEncode(body),
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 201) {
+        Map<String, dynamic> jsonResponse = json.decode(response.body);
+        Project project = Project.fromJson(jsonResponse);
+        return project;
+      }
+    } catch (e) {
+      // Handle any network errors
+      print('Error: $e');
+    }
+  }
+
+  Future<bool> deleteOneBookmark(int projectId, String token) async {
+    final Uri uri =
+        Uri.parse(API.baseUrl + API.bookmarkEndpoints.deleteOne(projectId));
+
+    try {
+      final http.Response response = await http.delete(
+        uri,
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer $token'
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return true;
+      }
+    } catch (e) {
+      // Handle any network errors
+      print('Error: $e');
+    }
+  }
+}
