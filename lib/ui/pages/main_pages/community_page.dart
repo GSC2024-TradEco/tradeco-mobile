406cc471bc330c21ed5b4f35a28b6b2a13934a08
Johanes Raphael Nandaputra <johanesraphaeln@gmail.com>
2024-02-24 19:15:39 +0700

    Add on refresh in community page

--- lib/ui/pages/main_pages/community_page.dart
+++ lib/ui/pages/main_pages/community_page.dart
@@ -46,94 +46,104 @@ class _CommunityPageState extends State<CommunityPage> {
     return DateFormat.yMMMMd().add_jms().format(dateTime);
   }
 
+  // Refresh method to reload posts
+  Future<void> _refreshPosts() async {
+    setState(() {
+      _fetchPostsFuture = _fetchPosts();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: _fetchPostsFuture,
-        builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else {
-            List<dynamic> postList = snapshot.data ?? [];
-            return ListView.separated(
-              itemBuilder: (context, index) {
-                final post = postList[index];
-                final user = post['User'];
-                return InkWell(
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.all(8),
-                    child: Container(
-                      padding: const EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white,
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              const Icon(Icons.person, size: 44),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(user['displayName']),
-                                  Text(_formatDateTime(post['createdAt'])),
-                                ],
-                              ),
-                            ],
-                          ),
-                          Container(
-                            width: double.infinity,
-                            margin: const EdgeInsets.all(8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
+      body: RefreshIndicator(
+        onRefresh: () => _refreshPosts(),
+        child: FutureBuilder(
+          future: _fetchPostsFuture,
+          builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              List<dynamic> postList = snapshot.data ?? [];
+              return ListView.separated(
+                itemBuilder: (context, index) {
+                  final post = postList[index];
+                  final user = post['User'];
+                  return InkWell(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Container(
+                        padding: const EdgeInsets.all(4),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
                               children: [
-                                Text(
-                                  post['title'],
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                                const Icon(Icons.person, size: 44),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(user['displayName']),
+                                    Text(_formatDateTime(post['createdAt'])),
+                                  ],
                                 ),
-                                const SizedBox(height: 5),
-                                Text(
-                                  post['description'],
-                                  style: const TextStyle(
-                                    fontSize: 16,
-                                  ),
-                                ),
-                                const SizedBox(height: 5),
-                                post['image'] != null
-                                    ? Image.network(post['image'])
-                                    : const SizedBox.shrink(),
-                                const SizedBox(height: 5),
-                                // const Row(
-                                //   children: [
-                                //     Icon(Icons.thumb_up_alt_outlined),
-                                //   ],
-                                // )
                               ],
                             ),
-                          )
-                        ],
+                            Container(
+                              width: double.infinity,
+                              margin: const EdgeInsets.all(8),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    post['title'],
+                                    style: const TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  Text(
+                                    post['description'],
+                                    style: const TextStyle(
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 5),
+                                  post['image'] != null
+                                      ? Image.network(post['image'])
+                                      : const SizedBox.shrink(),
+                                  const SizedBox(height: 5),
+                                  // const Row(
+                                  //   children: [
+                                  //     Icon(Icons.thumb_up_alt_outlined),
+                                  //   ],
+                                  // )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(height: 4);
-              },
-              itemCount: postList.length,
-            );
-          }
-        },
+                  );
+                },
+                separatorBuilder: (context, index) {
+                  return const SizedBox(height: 4);
+                },
+                itemCount: postList.length,
+              );
+            }
+          },
+        ),
       ),
     );
   }
